772508ce611518a8f39ab2bdd9104dc4f60d12c9
Rick Lupton <r.lupton@gmail.com>
2013-04-17 12:41:26 +0100

    Add setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
 import numpy as np
 
 setup(
-    cmdclass = {'build_ext': build_ext},
-    ext_modules = [Extension("assemble", ["assemble.pyx"], include_dirs=[np.get_include()])]
-)
+    name = "mbwind",
+    version = "0.0.1",
+    packages = find_packages(),
 
+    cmdclass = {'build_ext': build_ext},
+    ext_modules = [
+        Extension("assemble", ["mbwind/assemble.pyx"],
+                  include_dirs=[np.get_include()])
+    ]
+)
