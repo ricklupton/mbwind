2b4aca2b9f85f5dbd5425cd0c9ecb25ba5a6defd
Rick Lupton <r.lupton@gmail.com>
2013-04-19 17:59:57 +0100

    Add test for discont_trapz

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,5 +1,6 @@
-from mbwind.utils import skewmat, update_skewmat, rotmat_x, rotmat_y, rotmat_z
-from numpy import zeros, array, eye, pi, dot, sqrt
+from mbwind.utils import (skewmat, update_skewmat, rotmat_x, rotmat_y,
+                          rotmat_z, discont_trapz)
+from numpy import ones, zeros, array, arange, eye, pi, dot, sqrt
 from numpy.testing import assert_array_equal, assert_array_almost_equal
 from unittest import TestCase
 
@@ -36,3 +37,23 @@ class UtilsTestCase(TestCase):
                                   [sqrt(2), 1, 0])
         assert_array_almost_equal(dot(rotmat_z(pi / 4), [1, 1, 1]),
                                   [0, sqrt(2), 1])
+
+    def test_discont_trapz(self):
+        x = arange(0, 2.6, 0.5)
+
+        # Integrate constant value with constant weighting
+        y = ones((6, 7))
+        rho = ones((5, 2))
+        assert_array_equal(discont_trapz(y, rho, x), ones(7) * x[5])
+
+        # Integrate constant value over step function weighting
+        y = ones((6, 7))
+        rho = zeros((5, 2))
+        rho[:2] = 1
+        assert_array_equal(discont_trapz(y, rho, x), ones(7) * x[2])
+
+        # Integrate linear function between x2 and x4
+        y = x
+        rho = zeros((5, 2))
+        rho[2:4] = 1
+        assert_array_equal(discont_trapz(y, rho, x), (x[4]**2 - x[2]**2) / 2)
