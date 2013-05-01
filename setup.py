849800b12f45ff7f479cf60b7612a2af3769f0b2
Rick Lupton <r.lupton@gmail.com>
2013-05-01 10:52:57 +0100

    Update rearranged paths in setup.py

--- setup.py
+++ setup.py
@@ -4,13 +4,15 @@ from Cython.Distutils import build_ext
 import numpy as np
 
 setup(
-    name = "mbwind",
-    version = "0.0.1",
-    packages = find_packages(),
+    name="mbwind",
+    version="0.0.1",
+    packages=find_packages(),
 
-    cmdclass = {'build_ext': build_ext},
-    ext_modules = [
-        Extension("assemble", ["mbwind/assemble.pyx"],
-                  include_dirs=[np.get_include()])
+    cmdclass={'build_ext': build_ext},
+    ext_modules=[
+        Extension("mbwind.assemble", ["mbwind/assemble.pyx"],
+                  include_dirs=[np.get_include()]),
+        Extension("mbwind.elements._modal_calcs", ["mbwind/_modal_calcs.pyx"],
+                  include_dirs=[np.get_include()]),
     ]
 )
